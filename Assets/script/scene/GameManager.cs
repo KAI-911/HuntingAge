28b0bb70c9c07fef77ad240ffdfff448956587d2
KAI-911 <84615686+KAI-911@users.noreply.github.com>
2022-06-17 10:59:50 +0900

    クエストを村レベルごとのまとまりに分けて選択できるようにした。クエストの破棄もできるようになった。

--- Assets/script/scene/GameManager.cs
+++ Assets/script/scene/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] bool _chenge;
     [SerializeField] Scene _scene;
     [SerializeField] Quest _quest;
+    [SerializeField] Scene _villageScene;
+    public Scene VillageScene { get => _villageScene; }
     public Quest Quest { get => _quest; set => _quest = value; }
     void Start()
     {
