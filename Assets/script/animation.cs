5fb948641bd7ec932e6c9b420addc2039f749d74
DESKTOP-D0U92OG\master <kd1321210@st.kobedenshi.ac.jp>
2022-09-22 14:56:17 +0900

    村SE追加、ティラノSE追加

--- Assets/script/animation.cs
+++ Assets/script/animation.cs
@@ -6,12 +6,23 @@ public class animation : MonoBehaviour
 {
     [SerializeField] GameObject _effectPrefab;
     [SerializeField] GameObject _posObj;
+    [SerializeField] GameObject _soundPrefab;
+
+
     public void AnimationEvent(AnimationEvent animationEvent)
     {
-        if(animationEvent.stringParameter=="hit")
+        if (animationEvent.stringParameter == "hit")
         {
             var effect = Instantiate(_effectPrefab, _posObj.transform) as GameObject;
             Destroy(effect, 1);
+
+            if (animationEvent.objectReferenceParameter != null)
+            {
+                var obj = Instantiate(animationEvent.objectReferenceParameter) as GameObject;
+                obj.transform.position = transform.position;
+                obj.GetComponent<AudioSource>().Play();
+            }
+
         }
     }
 }
